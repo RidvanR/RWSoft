f4046804f03e7737f57716335a3dfc9f4e53b1ba
Ridvan <rridvan@icloud.com>
2024-12-14 19:46:16 +0100

    New Packages and dependencies

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import vuetify from './plugins/vuetify'; // Vuetify-Setup importieren
 import './style.css'; // Optional: Globale Styles
 import { scrollToTop } from './scrollToTop.js';
 import '@fortawesome/fontawesome-free/css/all.css';
+import 'intersection-observer';
 
 
 createApp(App).use(router).use(vuetify).mount('#app');
